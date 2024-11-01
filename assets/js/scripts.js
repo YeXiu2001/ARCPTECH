eb6b7d2cc9a9d40f8ef68ea208d3ee0de9ab2ea8
YeXiu2001 <raymartparadise2001@gmail.com>
2024-11-01 14:02:50 +0800

    updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,4 +9,35 @@ document.addEventListener('DOMContentLoaded', function() {
                 console.error('Element with id "site_header" not found.');
             }
         });
+
+
+        const element = document.querySelector(".typing-animation");
+        const text = element.textContent;
+        element.textContent = "";
+        let index = 0;
+        let isDeleting = false;
+    
+        function typeWriter() {
+            if (isDeleting) {
+                if (index > 0) {
+                    element.textContent = text.substring(0, index - 1);
+                    index--;
+                    setTimeout(typeWriter, 50); // Adjust deleting speed here
+                } else {
+                    isDeleting = false;
+                    setTimeout(typeWriter, 500); // Pause before typing again
+                }
+            } else {
+                if (index < text.length) {
+                    element.textContent += text.charAt(index);
+                    index++;
+                    setTimeout(typeWriter, 100); // Adjust typing speed here
+                } else {
+                    isDeleting = true;
+                    setTimeout(typeWriter, 1000); // Pause before deleting
+                }
+            }
+        }
+    
+        typeWriter();
 });
