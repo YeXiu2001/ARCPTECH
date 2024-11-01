ee78cf8575729a6f09f2cd6ad53927832b3dc4e8
YeXiu2001 <raymartparadise2001@gmail.com>
2024-11-01 15:50:14 +0800

    udpates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,39 +5,48 @@ document.addEventListener('DOMContentLoaded', function() {
             const headerElement = document.getElementById('site_header');
             if (headerElement) {
                 headerElement.innerHTML = data;
+                // Attach event listener after loading the header
+                document.getElementById('headercta').addEventListener('click', previewResume);
             } else {
                 console.error('Element with id "site_header" not found.');
             }
         });
 
+    const element = document.querySelector(".typing-animation");
+    const text = element.textContent;
+    element.textContent = "";
+    let index = 0;
+    let isDeleting = false;
 
-        const element = document.querySelector(".typing-animation");
-        const text = element.textContent;
-        element.textContent = "";
-        let index = 0;
-        let isDeleting = false;
-    
-        function typeWriter() {
-            if (isDeleting) {
-                if (index > 0) {
-                    element.textContent = text.substring(0, index - 1);
-                    index--;
-                    setTimeout(typeWriter, 50); // Adjust deleting speed here
-                } else {
-                    isDeleting = false;
-                    setTimeout(typeWriter, 500); // Pause before typing again
-                }
+    function typeWriter() {
+        if (isDeleting) {
+            if (index > 0) {
+                element.textContent = text.substring(0, index - 1);
+                index--;
+                setTimeout(typeWriter, 50); // Adjust deleting speed here
             } else {
-                if (index < text.length) {
-                    element.textContent += text.charAt(index);
-                    index++;
-                    setTimeout(typeWriter, 100); // Adjust typing speed here
-                } else {
-                    isDeleting = true;
-                    setTimeout(typeWriter, 1000); // Pause before deleting
-                }
+                isDeleting = false;
+                setTimeout(typeWriter, 500); // Pause before typing again
+            }
+        } else {
+            if (index < text.length) {
+                element.textContent += text.charAt(index);
+                index++;
+                setTimeout(typeWriter, 100); // Adjust typing speed here
+            } else {
+                isDeleting = true;
+                setTimeout(typeWriter, 1000); // Pause before deleting
             }
         }
-    
-        typeWriter();
+    }
+
+    typeWriter();
+
+    document.querySelectorAll('#phonecta').forEach(function(element) {
+        element.addEventListener('click', previewResume);
+    });
 });
+
+function previewResume() {
+    window.open('/assets/Paraiso_cv.pdf', '_blank');
+}
